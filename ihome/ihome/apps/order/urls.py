af4d182ddb12fd337316e4ccda02b4acdcad6f98
Your Name <44563674+OrigionalT@users.noreply.github.com>
2019-09-06 10:40:17 +0800

    V1.0 Finished

--- ihome/ihome/apps/order/urls.py
+++ ihome/ihome/apps/order/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/v1.0/orders/$', views.OrderView.as_view(), name='add'),
+    url(r'^api/v1.0/orders/$', views.OrderReservationView.as_view()),
+    url(r'^api/v1.0/orders/comment/$', views.OrderCommentView.as_view()),
 
 ]
--- ihome/ihome/apps/order/views.py
+++ ihome/ihome/apps/order/views.py
@@ -1,68 +1,223 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 import datetime
-import json
 
-from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-from django.views import View
+import json
+
+from django import http
+from django.shortcuts import render
+
+# Create your views here.
+from django.utils import timezone
+from django.views.generic.base import View
 
 from house.models import House
-from ihome.utils.views import LoginRequiredJSONMixin
 from order.models import Order
+from ihome.utils.views import LoginRequiredMixin
+from users.models import User
+
 from users.response_code import RET
 
 
-class OrderView(LoginRequiredJSONMixin, View):
-    def put(self,request):
-        pass
-    def get(self, request):
-        role = request.GET.get('role')
-        if not role:
-            return JsonResponse({ "errmsg": "参数错误", "errno": RET.PARAMERR})
-        if role is 'custom':
-            pass
-        elif role is 'landlord':
-            pass
-        data={}
-        orders=[]
-
-
-        return JsonResponse({"data": data, "errmsg": "OK", "errno": RET.OK})
+class OrderReservationView(LoginRequiredMixin, View):
+    """客户预定订单"""
 
     def post(self, request):
-        json_dict = json.loads(request.body)
+        """
+        用户添加订单
+        :param request:
+        :return:
+        """
+        # 1.获取前端传入的参数
+        json_dict = json.loads(request.body.decode())
         house_id = json_dict.get('house_id')
-        start_date = json_dict.get('start_date')
-        end_date = json_dict.get('end_date')
+        sd = json_dict.get('start_date')
+        ed = json_dict.get('end_date')
+        # 2. 校验参数
+        if not all([json_dict, house_id, sd, ed]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 2. 创建订单
+        user = request.user
+        # 创建时间 : 年月日时分秒
+        # ctime = timezone.localtime().strftime('%Y%m%d%H%M%S')
+        # 获取房屋信息
+        house = House.objects.get(id=house_id)
+        t_sd = datetime.datetime.strptime(sd, "%Y-%m-%d")
+        t_ed = datetime.datetime.strptime(ed, "%Y-%m-%d")
+        days = t_ed - t_sd
+        int_days = int(str(days)[0:1])
+        order = Order.objects.create(
+            user=user,
+            house=house,
+            begin_date=sd,
+            end_date=ed,
+            days=int_days,
+            house_price=house.price,
+            amount=house.price * int_days,
+            house_id=house.id,
+        )
+        order.order_id = order.id
+        # 3. 更新房屋数据
+        house.room_count -= 1
+        house.save()
 
-        if not all([house_id, end_date, start_date]):
-            return JsonResponse({
-                "errno": RET.PARAMERR,
-                "errmsg": "参数缺失"})
-        try:
-            rent_days = (datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%I:%S')
-                         - datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%I:%S')).days
-            house_info = House.objects.get(id=house_id)
-            order = Order.objects.create(
-                begin_date=start_date,
-                end_date=end_date,
-                days=rent_days,
-                house_price=int(house_info.price),
-                amount=int(rent_days * house_info.price + house_info.deposit),
-                status=Order.ORDER_STATUS_CHOICES[1],
-                comment='',
-                user_id=house_info.user_id,
-                house_id=house_id
-            )
-        except:
-            return JsonResponse({
-                "errno": RET.DBERR,
-                "errmsg": "数据库插入出错"})
-        data = order.id
-        return JsonResponse({"data": data,
-                             "errno": RET.OK,
-                             "errmsg": "OK"})
+        if house.room_count == 0:
+            return http.JsonResponse({'errno': RET.DATAERR, 'errmsg': '房源不足'})
+
+        # 18. 返回(json)
+        return http.JsonResponse({'code': "0",
+                                  'errmsg': 'ok',
+                                  'data': {'order_id': order.order_id}})
+
+    def get(self, request):
+        """
+        根据角色类型, 返回对应的结果
+        :param request:
+        :return:
+        """
+        role = request.GET.get("role")
+        user = request.user
+        if role == "custom":
+
+            try:
+                # 获取该用户的所有订单
+                orders = Order.objects.filter(user_id=user.id).order_by('-id')
+                # orders = user.order_set.all()
+            except Exception as e:
+                return http.JsonResponse({"errmsg": "fail",
+                                          "errno": 4101})
+            # 拼接返回
+            orders_res = []
+
+            # 遍历, 获取每个订单, 拼接参数
+            for order in orders:
+                house = order.house
+                order_dict = {
+                    "amount": order.amount,
+                    "comment": order.comment,
+                    "ctime": order.create_time,
+                    "start_date": order.begin_date,
+                    "end_date": order.end_date,
+                    "days": order.days,
+                    "img_url": house.index_image_url,
+                    "order_id": order.id,
+                    "status": order.status,
+                    "title": house.title
+                }
+
+                orders_res.append(order_dict)
+            return http.JsonResponse({"data": {"orders": orders_res},
+                                      "errmsg": "ok",
+                                      "errno": "0"})
+
+        if role == "landlord":
+            user_houses = user.house_set.all()
+            orders = Order.objects.filter(house_id__in=user_houses)
+
+            # 拼接返回
+            orders_res = []
+
+            # 遍历, 获取每个订单, 拼接参数
+            for order in orders:
+                house = order.house
+                order_dict = {
+                    "amount": order.amount,
+                    "comment": order.comment,
+                    "ctime": order.create_time,
+                    "start_date": order.begin_date,
+                    "end_date": order.end_date,
+                    "days": order.days,
+                    "img_url": house.index_image_url,
+                    "order_id": order.id,
+                    "status": order.status,
+                    "title": house.title
+                }
+
+                orders_res.append(order_dict)
+            return http.JsonResponse({"data": {"orders": orders_res},
+                                      "errmsg": "ok",
+                                      "errno": "0"})
+
+    def put(self, request):
+        """房东接单操作"""
+        json_dict = json.loads(request.body.decode())
+
+        action = json_dict.get("action")
+        order_id = json_dict.get("order_id")
+        reason = json_dict.get("reason")
+
+        # 保存信息到数据库
+        order = Order.objects.get(id=order_id)
+        house = order.house
+        # 判断action类型
+        if action == "accept":
+            order.status = Order.ORDER_STATUS_CHOICES[3][0]
+            order.save()
+            order_res = {
+                "comment": order.comment,
+                "ctime": order.create_time,
+                "days": order.days,
+                "start_date": order.begin_date,
+                "end_date": order.end_date,
+                "img_url": house.index_image_url,
+                "order_id": order.id,
+                "amount": order.amount,
+                "status": order.status,
+                "title": house.title
+            }
+            return http.JsonResponse({"errno": "0",
+                                      "errmsg": "ok",
+                                      "order_res": order_res})
+
+        elif action == "reject":
+            order.status = Order.ORDER_STATUS_CHOICES[6][0]
+            order.comment = reason
+            order.save()
+            order_res = {
+                "comment": reason,
+                "ctime": order.create_time,
+                "days": order.days,
+                "start_date": order.begin_date,
+                "end_date": order.end_date,
+                "img_url": house.index_image_url,
+                "order_id": order.id,
+                "amount": order.amount,
+                "status": order.status,
+                "title": house.title
+            }
+            return http.JsonResponse({"errno": "0",
+                                      "errmsg": "ok",
+                                      "order_res": order_res})
+
+
+class OrderCommentView(LoginRequiredMixin, View):
+    """评价订单"""
+
+    def put(self, request):
+        json_dict = json.loads(request.body.decode())
+        comment = json_dict.get("comment")
+        order_id = json_dict.get("order_id")
+        # 根据order_id获取id
+        order = Order.objects.get(id=order_id)
+        # house = order.house.get(id=order.house_id)
+        order.comment = comment
+        order.status = Order.ORDER_STATUS_CHOICES[4][0]
+        order.save()
+        # order_res = {
+        #     "comment": order.comment,
+        #     "ctime": order.create_time,
+        #     "days": order.days,
+        #     "start_date": order.begin_date,
+        #     "end_date": order.end_date,
+        #     "img_url": house.index_image_url,
+        #     "order_id": order.id,
+        #     "amount": order.amount,
+        #     "status": order.status,
+        #     "title": house.title
+        # }
+
+        return http.JsonResponse({
+            'errno': 0,
+            'errmsg': 'OK'
+        })
